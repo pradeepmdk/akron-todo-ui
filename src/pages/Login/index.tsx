ee6cab8836f1842c7fa27b7d4e99e20bbdf0bf46
pradeepkumar <pradeepkumar@pradeepkumars-MacBook-Pro.local>
2023-02-11 10:31:46 +0530

    update feild

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -60,7 +60,7 @@ export default function Login() {
                             fullWidth
                             required
                             name="email"
-                            label="User Name s"
+                            label="User Name "
                             type="text"
                         />
                         <TextField
