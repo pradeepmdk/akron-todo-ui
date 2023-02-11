79c838094fa2d1ef2de4903bea8d5c94562d7116
pradeepkumar <pradeepkumar@pradeepkumars-MacBook-Pro.local>
2023-02-11 10:39:03 +0530

    sdfsdfs

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -60,7 +60,7 @@ export default function Login() {
                             fullWidth
                             required
                             name="email"
-                            label="User Namesdfsdfsdsd "
+                            label="User Namesdfsdfsdsdsdfsd "
                             type="text"
                         />
                         <TextField
